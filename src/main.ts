86b623ff3aee69f706fd8c3ae9c932dca60af4fd
sowmya-dot <swmgunturu@gmail.com>
2024-08-13 15:35:06 +0530

    fourth commint

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000', // Your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // Enable CORS
+  app.enableCors();
 
   await app.listen(3000);
 }
