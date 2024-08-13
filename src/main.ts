16c73bdc9018c61a0562107785dc8693d7e0d024
sowmya-dot <swmgunturu@gmail.com>
2024-08-13 15:15:24 +0530

    fourth commint

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with specific settings
   app.enableCors({
-    origin: 'https://task-tracker-frontend-2pea.onrender.com',  // Replace with your Netlify frontend URL
+    origin: 'http://localhost:3000', // Your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // if you want to allow cookies
+    credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
